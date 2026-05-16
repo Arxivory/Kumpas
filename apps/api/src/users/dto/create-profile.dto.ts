2e80199f28409e8a0d23770fa1a0e3000ebaf00a
Vince <babyaivan86@gmail.com>
2026-05-16 13:54:58 +0800

    fix: assigned all fields as mandatory and not nullable

--- apps/api/src/users/dto/create-profile.dto.ts
+++ apps/api/src/users/dto/create-profile.dto.ts
@@ -1,7 +1,7 @@
 export class CreateProfileDto {
   id: string;
   email: string;
-  username?: string;
-  firstName?: string;
-  lastName?: string;
+  username: string;
+  firstName: string;
+  lastName: string;
 }
