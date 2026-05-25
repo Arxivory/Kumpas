8405977e480fd326d4e443f2b01274e40e74e0cf
Vince <babyaivan86@gmail.com>
2026-05-25 18:31:06 +0800

    delete: removed a test route

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -5,31 +5,4 @@ import { PrismaService } from './prisma.service';
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
-  @Get('seed-vince')
-  async seedTestUser() {
-    try {
-      const testUser = await this.prisma.user.upsert({
-        where: { email: 'vince.test@kumpas.dev' },
-        update: {},
-        create: {
-          id: 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
-          email: 'vince.test@kumpas.dev',
-          username: 'vince_monorepo_test',
-          firstName: 'Vince',
-          lastName: 'Dev',
-        },
-      });
-      
-      return {
-        status: 'Success!',
-        message: 'NestJS successfully talked to Supabase using Prisma 7!',
-        data: testUser,
-      };
-    } catch (error) {
-      return {
-        status: 'Database Connection Error',
-        error: error.message,
-      };
-    }
-  }
 }
